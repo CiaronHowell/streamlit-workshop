074a14c7298c67ce899d9b731d3ea3bc8b33f21c
Howell <ciaron.howell@stfc.ac.uk>
2021-09-20 11:21:06 +0100

    Added dynamic content

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title('My first Streamlit app')
 
+st.markdown('## Static Content')
 # Can output different types
 a = 12.34
 st.write(a)
@@ -35,3 +36,25 @@ int main() {
 ```
 ''')
 
+st.markdown('## Dynamic Content')
+
+st.checkbox('Tick me')
+
+if st.checkbox('Tick me to see'):
+    st.write(42)
+
+selection = st.selectbox('Choose an option', ["Nothing here", "or here", "but something here"])
+
+if selection == "but something here":
+    st.write("You found me!")
+else:
+    st.write("Try again")
+
+int_val = st.slider("Pick an integer", min_value=1, max_value=10, value=5)
+st.write(int_val)
+
+float_val = st.slider("Pick an float", min_value=1.0, max_value=10.0, value=5.0, step=0.1, format="%.1f")
+st.write(float_val)
+
+# Layout
+st.sidebar.write("Adding texst to the sidebar...")
