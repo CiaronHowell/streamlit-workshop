d7b2b7e5559f9b64223cf7b68239f5f7761e7be0
Howell <ciaron.howell@stfc.ac.uk>
2021-09-20 12:27:28 +0100

    Missed port in config

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 mkdir -p ~/.streamlit/
-printf "[server]\nheadless=true\nenableCORS=false\n" "${PORT}" > ~/.streamlit/config.toml
+printf "[server]\nheadless=true\nenableCORS=false\nport=%s\n" "${PORT}" > ~/.streamlit/config.toml
