e4a91faea12dceb433056a9dbb47906ff7cdd27a
Howell <ciaron.howell@stfc.ac.uk>
2021-09-20 11:12:36 +0100

    Added static content

--- app.py
+++ app.py
@@ -1,3 +1,37 @@
 import streamlit as st
 
 st.title('My first Streamlit app')
+
+# Can output different types
+a = 12.34
+st.write(a)
+
+st.write('Some text here')
+
+# Using markdown
+st.markdown('Text can be **bold** and _italic_.')
+st.markdown(r'''Raw strings can be very useful: we can
+- create
+- bullet
+- lists
+
+and other markdown things like horizontal lines
+---
+''' )
+
+# Can display latex - also a latex specific method
+st.markdown(r'''
+$$
+i\hbar\frac{\partial}{\partial t} \Psi(x,t) = \left [ - \frac{\hbar^2}{2m}\frac{\partial^2}{\partial x^2} + V(x,t)\right ] \Psi(x,t)
+$$
+''')
+
+# As we can use MD we have access to code blocks
+st.markdown(r'''
+```c
+int main() {
+    return 0;
+}
+```
+''')
+
